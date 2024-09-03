0d2eefd9a6422da47145f98ad1bf78a4d3373b6d
vincpinas <ikbengerrit92@gmail.com>
2024-09-03 10:46:08 +0200

    mini port updated

--- src/Components/MiniPortfolio/MiniPortfolio.tsx
+++ src/Components/MiniPortfolio/MiniPortfolio.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './MiniPortfolio.scss';
 import * as TB from 'react-icons/tb';
 import moment from 'moment';
@@ -8,16 +8,15 @@ import CV from '../../Assets/CV.pdf';
 import { Link } from 'react-router-dom';
 
 function MiniPortfolio() {
+  const [avatar, setAvatar] = useState<string>("")
   const ref = useRef<HTMLSpanElement>(null);
-  const avatar = () => {
-    let temp: string = "";
-    const d = new Date().getDay();
-    avatars.forEach((avatar, index) => {
-      if(d === index) temp = avatar;
-    });
 
-    return temp;
-  }
+  useEffect(() => {
+    const day = new Date().getDay();
+    avatars.forEach((avatar, index) => {
+      if (day === index) setAvatar(avatar);
+    });
+  }, [])
 
   function setAnimation() {
     ref.current!.style.animationName = 'circle-expand'
@@ -32,21 +31,20 @@ function MiniPortfolio() {
     }, 10);
   }
 
-
   return (
     <div id='miniPortComponent'>
       <h5 className='hoverInfo'>Hover over any of the bottom 6 <br /> elements here for tooltips!</h5>
       <h5 className='hoverInfo avatar'>This avatar changes per day <br /> so check in tomorrow!</h5>
       <Link to='/about' className='imgWrapper' onMouseEnter={setAnimation} onMouseLeave={resetAnimation}>
         <span className="innerBorder" ref={ref} />
-        <img src={avatar()} alt="avatar" />
+        <img src={avatar} alt="avatar" />
       </Link>
       <div className="infoHeader">
         <h3>Vincent Pinas</h3>
         <h5>Webdeveloper</h5>
       </div>
       <div className='socialsHeader'>
-        <a href="https://www.instagram.com/shaftattack/" target="_blank" rel="noreferrer">
+        <a href="https://www.instagram.com/vpins101/" target="_blank" rel="noreferrer">
           <TB.TbBrandInstagram />
         </a>
         <a href="https://nl.linkedin.com/in/vincent-pinas-a4656b223/" target="_blank" rel="noreferrer">
@@ -59,20 +57,16 @@ function MiniPortfolio() {
       <div className='detailsHeader'>
         <Tooltip text='Since start of school in 2019'>
           <h5>{moment().diff("20190715", "years", false)}</h5>
-          <h5>Years of <br /> work</h5>
+          <h5>Years of <br /> developing</h5>
         </Tooltip>
-        <Tooltip text='Only counts work for real clients'>
-          <h5>2+</h5>
+        <Tooltip text='Only counts work for real clients or products that go in production'>
+          <h5>2</h5>
           <h5>Completed <br /> projects</h5>
-        </Tooltip>
-        <Tooltip text='Only counts real clients'>
-          <h5>1+</h5>
-          <h5>Satisfied <br /> customers</h5>
         </Tooltip>
       </div>
       <div className="miscHeader">
-        <Tooltip text='Last updated: 29/09/2022' className='cvWrapper'>
-          <a href={CV} download="CV Vincent Pinas - 2022">Download CV <TB.TbDownload /></a>
+        <Tooltip text={`Last updated: 03/09/2024`} className='cvWrapper'>
+          <a href={CV} download={`CV Vincent Pinas - 03/09/2024`}>Download CV <TB.TbDownload /></a>
         </Tooltip>
         <div className='miscSocials'>
           <Tooltip text='You can contact me on WhatsApp 😇'>
